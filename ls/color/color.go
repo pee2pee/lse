e328be1699f5c6290e53cbf6720f8cee7f02b7d1
Clement Sam <clementsam75@gmail.com>
2022-10-21 03:31:40 +0000

    add color and table libraries (#12)

---
+++ ls/color/color.go
@@ -0,0 +1,95 @@
+package color
+
+import (
+	"io"
+	"os"
+	"strings"
+
+	"github.com/mattn/go-colorable"
+	"github.com/mgutz/ansi"
+)
+
+var (
+	_isColorEnabled = true
+	checkedNoColor  = false
+)
+
+type Palette struct {
+	// Magenta outputs ANSI color if stdout is a tty
+	Magenta func(string) string
+	// Cyan outputs ANSI color if stdout is a tty
+	Cyan func(string) string
+	// Red outputs ANSI color if stdout is a tty
+	Red func(string) string
+	// Yellow outputs ANSI color if stdout is a tty
+	Yellow func(string) string
+	// Blue outputs ANSI color if stdout is a tty
+	Blue func(string) string
+	// Green outputs ANSI color if stdout is a tty
+	Green func(string) string
+	// Gray outputs ANSI color if stdout is a tty
+	Gray func(string) string
+	// Bold outputs ANSI color if stdout is a tty
+	Bold func(string) string
+}
+
+func Color() *Palette {
+	return &Palette{
+		Magenta: makeColorFunc("magenta"),
+		Cyan:    makeColorFunc("cyan"),
+		Red:     makeColorFunc("red"),
+		Yellow:  makeColorFunc("yellow"),
+		Blue:    makeColorFunc("blue"),
+		Green:   makeColorFunc("green"),
+		Gray:    makeColorFunc("black+h"),
+		Bold:    makeColorFunc("default+b"),
+	}
+}
+
+// NewColorable returns an output stream that handles ANSI color sequences on Windows
+func NewColorable(out io.Writer) io.Writer {
+	if outFile, isFile := out.(*os.File); isFile {
+		return colorable.NewColorable(outFile)
+	}
+	return out
+}
+
+func makeColorFunc(color string) func(string) string {
+	cf := ansi.ColorFunc(color)
+	return func(arg string) string {
+		if isColorEnabled() && isStdoutTerminal() {
+			return cf(arg)
+		}
+		return arg
+	}
+}
+
+func EnableColor(enable bool) {
+	checkedNoColor = true
+	_isColorEnabled = enable
+}
+
+func isColorEnabled() bool {
+	if !checkedNoColor {
+		_, _isColorEnabled = os.LookupEnv("NO_COLOR")
+		_isColorEnabled = !_isColorEnabled // Revert the value NO_COLOR disables color
+
+		if !_isColorEnabled {
+			_isColorEnabled = os.Getenv("COLOR_ENABLED") == "1" || os.Getenv("COLOR_ENABLED") == "true"
+		}
+		checkedNoColor = true
+	}
+	return _isColorEnabled
+}
+
+func Is256ColorSupported() bool {
+	term := os.Getenv("TERM")
+	colorterm := os.Getenv("COLORTERM")
+
+	return strings.Contains(term, "256") ||
+		strings.Contains(term, "24bit") ||
+		strings.Contains(term, "truecolor") ||
+		strings.Contains(colorterm, "256") ||
+		strings.Contains(colorterm, "24bit") ||
+		strings.Contains(colorterm, "truecolor")
+}
---
+++ ls/color/terminal.go
@@ -0,0 +1,75 @@
+package color
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/mattn/go-isatty"
+	"golang.org/x/sys/execabs"
+	"golang.org/x/term"
+)
+
+var (
+	_isStdoutTerminal = false
+	checkedTerminal   = false
+)
+
+func isStdoutTerminal() bool {
+	if !checkedTerminal {
+		_isStdoutTerminal = IsTerminal(os.Stdout)
+		checkedTerminal = true
+	}
+	return _isStdoutTerminal
+}
+
+// IsTerminal reports whether the file descriptor is connected to a terminal
+var IsTerminal = func(f *os.File) bool {
+	return isatty.IsTerminal(f.Fd()) || IsCygwinTerminal(f)
+}
+
+func IsCygwinTerminal(f *os.File) bool {
+	return isatty.IsCygwinTerminal(f.Fd())
+}
+
+var TerminalSize = func(w interface{}) (int, int, error) {
+	if f, isFile := w.(*os.File); isFile {
+		return term.GetSize(int(f.Fd()))
+	}
+
+	return 0, 0, fmt.Errorf("%v is not a file", w)
+}
+
+func isCygwinTerminal(w io.Writer) bool {
+	if f, isFile := w.(*os.File); isFile {
+		return IsCygwinTerminal(f)
+	}
+	return false
+}
+
+func TerminalWidth(out io.Writer) int {
+	defaultWidth := 80
+
+	if w, _, err := TerminalSize(out); err == nil {
+		return w
+	}
+
+	if isCygwinTerminal(out) {
+		tputExe, err := execabs.LookPath("tput")
+		if err != nil {
+			return defaultWidth
+		}
+		tputCmd := exec.Command(tputExe, "cols")
+		tputCmd.Stdin = os.Stdin
+		if out, err := tputCmd.Output(); err == nil {
+			if w, err := strconv.Atoi(strings.TrimSpace(string(out))); err == nil {
+				return w
+			}
+		}
+	}
+
+	return defaultWidth
+}
