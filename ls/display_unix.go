1d01d035ccde5867613dcfc4e3d4ac8437e7d160
Clement Sam <clementsam75@gmail.com>
2022-10-25 01:43:44 +0000

    support long listing on windows

---
+++ ls/display_unix.go
@@ -0,0 +1,31 @@
+//go:build !windows
+
+package ls
+
+import (
+	"os/user"
+	"strconv"
+	"syscall"
+
+	"github.com/profclems/glab/pkg/tableprinter"
+)
+
+func (l *LS) _display(table *tableprinter.TablePrinter, name string, dir *Dir) (int, error) {
+	stat := dir.Info.Sys().(*syscall.Stat_t)
+
+	usr, err := user.LookupId(strconv.Itoa(int(stat.Uid)))
+	if err != nil {
+		return 0, err
+	}
+
+	group, err := user.LookupGroupId(strconv.Itoa(int(stat.Gid)))
+	if err != nil {
+		return 0, err
+	}
+
+	timeStr := dir.Info.ModTime().UTC().Format("Jan 02 15:04")
+
+	table.AddRow(dir.Info.Mode(), stat.Nlink, usr.Username, group.Name, dir.Info.Size(), timeStr, name)
+
+	return int(stat.Blocks), nil
+}
